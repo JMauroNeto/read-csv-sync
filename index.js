4e861af8ff91ea49ab50f3a73eaa13c02d59363b
José Mauro Neto <jmauroneto@gmail.com>
2020-09-01 13:18:08 -0300

    npm setup and code

---
+++ index.js
@@ -0,0 +1,23 @@
+import { readFileSync, writeFileSync } from 'fs';
+
+export const readCSVSync = (path) => {
+    const data = readFileSync(path, 'utf-8').split('\n');
+
+    const keys = data[0].split(',');
+
+    const formattedData = [];
+
+    for(let i=1; i<data.length; i++){
+        const row = data[i].split(',');
+
+        let rowObject = {};
+
+        keys.forEach((key, index) => {
+            rowObject[key] = row[index];
+        })
+
+        formattedData.push(rowObject);
+    };
+
+    return formattedData;
+}
